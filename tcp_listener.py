97121f5eaef793213ca97009c3e2b3747b7bed61
BuildTools <unconfigured@null.spigotmc.org>
2021-03-08 22:12:23 +0200

    init commit

---
+++ tcp_listener.py
@@ -0,0 +1,50 @@
+import os
+import time
+import subprocess
+
+blacklist = input("Your own ip: ").replace(" ", "")
+
+def banner():
+    print(""" 
+████████╗ ██████╗██████╗               ███████╗ █████╗ ██████╗ 
+╚══██╔══╝██╔════╝██╔══██╗              ██╔════╝██╔══██╗██╔══██╗
+   ██║   ██║     ██████╔╝    █████╗    █████╗  ███████║██████╔╝
+   ██║   ██║     ██╔═══╝     ╚════╝    ██╔══╝  ██╔══██║██╔══██╗
+   ██║   ╚██████╗██║                   ███████╗██║  ██║██║  ██║
+   ╚═╝    ╚═════╝╚═╝                   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝                                                          
+    -- Handcraftef with love, by Vsimpro <3
+    """)
+
+def port22():
+    command = 'sudo tcpdump -nlq "tcp[13] == 2 and dst port 22" | while read x; do echo "${x}"; echo -en \\a; done'
+    os.system("clear")
+    banner()
+    inward = subprocess.Popen(command, stdout=subprocess.PIPE,shell=True)
+    while True:
+        line = inward.stdout.readline()
+        line = line.decode("utf-8")
+        if line:
+            log_dict(line, "22")
+    
+def write(data):
+    print("[+]", data)
+    with open("log.txt", "a+", encoding="utf-8") as file:
+        file.write(data)
+
+def log_dict(echo, port):
+    if echo == "\n":
+        return
+    if echo == "-en a\n":
+        return
+    if echo == " ":
+        return
+
+    data = {}
+    datapoint = echo.split(" ")
+    time = datapoint[0]
+    ip = datapoint[2]
+    if blacklist in ip:
+        return
+    n_ip = ip.split(".")    
+    n_ip = f"{ip[0]}.{ip[1]}.{ip[2]}.{ip[3]}"
+    write(f"{n_ip}:{port} -- {time}\n")
